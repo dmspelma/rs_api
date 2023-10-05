00276850c90d3be27d7e30defb9a1cbe4c4d224c
Drew Spelman <drewspelman@gmail.com>
2023-10-05 09:27:50 -0700

    fix rspec linter for rs_api version RsMonthlyXp

--- spec/lib/rs_api/runemetrics/monthly_xp_spec.rb
+++ spec/lib/rs_api/runemetrics/monthly_xp_spec.rb
@@ -6,42 +6,42 @@ module RsApi
 
     describe RsMonthlyXp do
       it 'with invalid name' do
-        error = RsApi::CheckValidPlayerName::RsNameInvalid
+        error = PlayerNameHelper::RsNameInvalid
         expect { described_class.new('tib...dragon') }.to raise_error(error)
       end
 
       it 'with valid name' do
         expect { described_class.new('tibthedragon') }.not_to raise_error
       end
-    end
 
-    describe 'Requesting monthly xp data for player' do
-      it 'returns MissingPlayerData' do
-        player_name = 'missing'
-        VCR.use_cassette('runemetrics/missing_player_data', erb: { player_name: }) do
-          service = RsMonthlyXp.new(player_name)
-          error = RsApi::RsMonthlyXp::MissingPlayerData
-          expect { service.raw_data }.to raise_error(error)
+      describe 'Requesting monthly xp data for player' do
+        it 'returns MissingPlayerData' do
+          player_name = 'missing'
+          VCR.use_cassette('runemetrics/missing_player_data', erb: { player_name: }) do
+            service = described_class.new(player_name)
+            error = RsApi::RsMonthlyXp::MissingPlayerData
+            expect { service.raw_data }.to raise_error(error)
+          end
         end
-      end
 
-      it 'returns PlayerNotFound' do
-        player_name = 'not found'
-        VCR.use_cassette('runemetrics/player_not_found', erb: { player_name: }) do
-          service = RsMonthlyXp.new(player_name)
-          error = RsApi::RsMonthlyXp::PlayerNotFound
-          expect { service.raw_data }.to raise_error(error)
+        it 'returns PlayerNotFound' do
+          player_name = 'not found'
+          VCR.use_cassette('runemetrics/player_not_found', erb: { player_name: }) do
+            service = described_class.new(player_name)
+            error = RsApi::RsMonthlyXp::PlayerNotFound
+            expect { service.raw_data }.to raise_error(error)
+          end
         end
-      end
 
-      it 'returns monthly xp data for all skills' do
-        player_name = 'tibthedragon'
-        VCR.use_cassette('runemetrics/player_found', erb: { player_name: }) do
-          service = RsMonthlyXp.new(player_name)
-          data = service.raw_data
-          expect { data }.not_to raise_error
-          expect(data.class).to eq(Hash)
-          expect(data.length).to eq(MONTHLY_XP_SKILL_ID_CONST.length)
+        it 'returns monthly xp data for all skills' do
+          player_name = 'tibthedragon'
+          VCR.use_cassette('runemetrics/player_found', erb: { player_name: }) do
+            service = described_class.new(player_name)
+            data = service.raw_data
+            expect { data }.not_to raise_error
+            expect(data.class).to eq(Hash)
+            expect(data.length).to eq(MONTHLY_XP_SKILL_ID_CONST.length)
+          end
         end
       end
     end

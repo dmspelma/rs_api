4b200cd2cd3df619198944bc5370336e735fb72c
Drew Spelman <drewspelman@gmail.com>
2023-10-26 23:27:59 -0700

    test fix for ServiceUnavailable

--- spec/lib/rs_api/request_spec.rb
+++ spec/lib/rs_api/request_spec.rb
@@ -3,28 +3,54 @@
 module RsApi
   class RsRequestTest
     RSpec.describe RsRequest do
-      let(:request) { described_class.new(url, params) }
+      let(:request) { described_class.new(url, params('player')) }
 
       it 'initialize' do
         expect(request.url).not_to be_nil
-        expect(request.params.class).to eq(params.class)
+        expect(request.params).not_to be_nil
       end
 
-      it 'get request successful' do
-        full_url = URI(url)
-        full_url.query = URI.encode_www_form(params)
-        expected_response = { body: 'success' }
-        stub_request(:get, full_url).to_return(expected_response)
+      context 'when get' do
+        it 'successful' do
+          VCR.use_cassette('rs_request__success') do
+            response = request.get
 
-        response = request.get
+            expect(response).not_to be_nil
+            expect(response.code).to eq('200')
+          end
+        end
 
-        expect(response).not_to be_nil
-        expect(response.body).to eq(expected_response[:body])
-        expect(response.code).to eq('200')
+        it 'error: player not found' do
+          not_found_name = 'not found'
+          erb = { player_name: not_found_name }
+          VCR.use_cassette('rs_request__player_not_found', erb:) do
+            service = described_class.new(url, params(not_found_name))
+
+            expect { service.get }.to raise_error player_not_found_error
+          end
+        end
+
+        it 'error: service unavailable' do
+          unavailable_name = 'unavailable'
+          erb = { player_name: unavailable_name }
+          VCR.use_cassette('rs_request__service_unavailable', erb:, allow_playback_repeats: true) do
+            service = described_class.new(url, params(unavailable_name))
+
+            expect { service.get }.to raise_error service_unavailable_error
+          end
+        end
       end
 
-      def params
-        { player: 'player' }
+      def params(name)
+        { player: name }
+      end
+
+      def player_not_found_error
+        RsApi::RsRequest::PlayerNotFound
+      end
+
+      def service_unavailable_error
+        RsApi::RsRequest::ServiceUnavailable
       end
 
       def url

a6b424671959b8e830b082df43c8b58003c40613
Drew Spelman <drewspelman@gmail.com>
2023-11-01 23:32:08 -0700

    Move config to initialize file

--- lib/rs_api.rb
+++ lib/rs_api.rb
@@ -10,6 +10,7 @@ require 'net/http'
 require 'text-table'
 require 'uri'
 require 'yaml'
+require_relative '../config/initializers/config'
 require_relative 'rs_api/patches/integer'
 require_relative 'rs_api/patches/string'
 require_relative 'rs_api/patches/text_table__cell_patch'
@@ -17,25 +18,16 @@ require_relative 'rs_api/patches/text_table__table_patch'
 
 # Base RsApi module for autoloading files
 module RsApi
-  def self.autoload_files(*sub_dir)
-    sub_dir.each do |s|
+  FOLDERS_TO_LOAD = [:helpers, :hiscores, :runemetrics, ''].freeze
+
+  def self.autoload_files
+    FOLDERS_TO_LOAD.each do |s|
       Dir[File.join(__dir__, 'rs_api', s.to_s, '*.rb')].each do |file|
         class_name = File.basename(file, '.rb').camelcase
         autoload class_name, file
       end
     end
   end
-
-  Config.setup do |config|
-    env = ENV['RS_API_ENV'] || 'development'
-
-    # Set preferred settings
-    config.const_name = 'Settings'
-    config.evaluate_erb_in_yaml = true
-
-    # Load Settings
-    config.load_and_set_settings(Config.setting_files('config', env))
-  end
 end
 
-RsApi.autoload_files(:helpers, :hiscores, :runemetrics, '')
+RsApi.autoload_files
